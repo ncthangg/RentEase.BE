e740904f842cd2ad3e7b7228ee73d172e79c1230
Nguyen Viet Nam Khanh <nguyenvietnamkhanhbl@gmail.com>
2025-03-08 13:46:02 +0700

    Update

--- RentEase.API/Program.cs
+++ RentEase.API/Program.cs
@@ -23,14 +23,11 @@ builder.Services.AddControllers().AddJsonOptions(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "RentEASE API V1");
     });
-}
 
 ////Use Session
 //app.UseSession();
